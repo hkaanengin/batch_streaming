9b23db748be47925d244e4fe19729a1f3fbc17b8
hkaanengin <56260437+hkaanengin@users.noreply.github.com>
2023-06-14 23:10:07 +0300

    comments removed

--- parq_to_pandas.py
+++ parq_to_pandas.py
@@ -12,17 +12,3 @@ def Parq_to_pandas():
 
     pd=parqDF.toPandas()
     return pd 
-
-# spark = SparkSession.builder.getOrCreate()
-
-# parqDF=spark.read.parquet("measurements.parquet")
-# parqDF.createOrReplaceTempView("TempHum")
-# parqDF.printSchema()
-# parqDF.show(truncate=False)
-
-# pd=parqDF.toPandas()
-# print(len(pd))
-# print(type(pd.iat[0,0]))
-# print(type(pd.iat[0,1]))
-# print(type(pd.iat[0,2]))
-# print(type(pd.iat[0,3]))
